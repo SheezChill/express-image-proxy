d40e67b2b2da9a470ab33054c3467b16284088f0
SheezChill <rageendergaming@gmail.com>
2022-12-11 20:49:31 +0700

    🧹 cleaned code

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,36 @@ import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3333
 
-const allowedOrigins = [/addict-next.vercel.app/, /localhost/]
+const whitelist = [/addict-next.vercel.app/, /localhost/]
 app.use(
   cors({
-    origin: allowedOrigins
+    origin: whitelist
   })
 )
-// app.use(cors({ origin: true }))
 
-app.get('/', function (req, res) {
-  res.send('lol')
+app.get('/', (req, res) => {
+  res.send('/image?url=[url]')
 })
 
-app.get('/image', function (req, res) {
+app.get('/image', (req, res) => {
   const url = req.query.url as string
 
-  const request = https.get(url, function (response) {
+  const request = https.get(url, (response) => {
     const contentType = response.headers['content-type']
+    if (!contentType) {
+      res.send('error')
+      return
+    }
 
-    res.setHeader('Content-Type', contentType as string)
+    res.setHeader('Content-Type', contentType)
 
     response.pipe(res)
   })
 
-  request.on('error', function (e) {
+  request.on('error', (e) => {
     console.error(e)
   })
 })
-
-app.listen(3000)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
