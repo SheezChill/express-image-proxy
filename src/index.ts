97b3858effb184ed33a8a231066cea5b1948fdbc
SheezChill <rageendergaming@gmail.com>
2022-12-11 13:41:55 +0700

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import express from 'express'
 import https from 'https'
+import cors from 'cors'
 
 const app = express()
 const port = process.env.PORT || 3333
+
+const allowedOrigins = ['addict-next.vercel.app', 'localhost']
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.'
+        return callback(new Error(msg), false)
+      }
+      return callback(null, true)
+    }
+  })
+)
 
 app.get('/', function (req, res) {
   res.send('lol')
